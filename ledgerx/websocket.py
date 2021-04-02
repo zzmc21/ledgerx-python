afa30a7b5649bc659aa2c41f5ceddb78b3ffc7fd
Rob Egan <ledgerx-rob@egannetworks.com>
2021-04-01 19:24:50 -0700

    fixed logging

--- ledgerx/websocket.py
+++ ledgerx/websocket.py
@@ -5,27 +5,25 @@ import json
 
 from ledgerx.util import gen_websocket_url
 
-logging.basicConfig(level=logging.INFO)
-
 class WebSocket:
 
     connection = None
         
     def connect(self, include_api_key : bool = False) -> websockets.client.WebSocketClientProtocol:
         websocket_resource_url = gen_websocket_url(include_api_key)
-        logging.info(f"Connecting to {websocket_resource_url}")
+        logging.debug(f"Connecting to {websocket_resource_url}")
         self.connection = websockets.connect(websocket_resource_url)
         logging.info(f"Connected {self.connection}")
 
     async def close(self):
-        logging.info(f"Closing connection {self.connection}")
+        logging.debug(f"Closing connection {self.connection}")
         async with self.connection as websocket:
             await websocket.close()
         self.connection = None
         logging.info(f"Closed connection {self.connection}")
 
     def update_book_top(self, data):
-        logging.info(f"Received: {data}")
+        logging.debug(f"Received: {data}")
 
     def update_by_type(self, data):
         type = data['type']
@@ -39,7 +37,7 @@ class WebSocket:
     async def consumer_handle(self, websocket: websockets.client.WebSocketClientProtocol) -> None:
         logging.info(f"consumer_handle starting: {websocket}")
         async for message in websocket:
-            #logging.info(f"Received: {message}")
+            logging.debug(f"Received: {message}")
             data = json.loads(message)
             if 'type' in data:
                 self.update_by_type(data)
